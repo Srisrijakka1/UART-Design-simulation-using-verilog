ea7f752594bca8cbd748d92da2a683954632018f
SRISRI JAKKA <106643865+Srisrijakka1@users.noreply.github.com>
2022-11-03 14:20:15 +0530

    Update baudratetb.sv

--- baudratetb.sv
+++ baudratetb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 30.10.2022 15:14:04
-// Design Name: 
-// Module Name: baudratetb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module baudratetb();
     reg rst,clk;
