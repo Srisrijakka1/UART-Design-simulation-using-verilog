c96a7eeaba6314ea0dd44aef11cb05db1b2ce5db
SRISRI JAKKA <106643865+Srisrijakka1@users.noreply.github.com>
2022-11-03 14:18:54 +0530

    Update Rx_tb.sv

--- Rx_tb.sv
+++ Rx_tb.sv
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02.11.2022 23:07:25
-// Design Name: 
-// Module Name: Rx_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Rx_tb();
     reg clk,rst,rxd;
     wire rxd_done,tick;
